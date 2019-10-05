21966e659255ad0a65acf308be733ea61201bfa1
Andrew Zigler <andrewzigler@gmail.com>
2019-10-05 12:23:57 -0700

    Continue testing Webhook

--- src/WebSocketManager.js
+++ src/WebSocketManager.js
@@ -34,7 +34,7 @@ class WebSocketManager extends require('ws').Server {
         connection.server = this.server
         // send server status to client
         this.server.ribbitSend({ socket: connection }, {
-          id: 'server.online-YAS07',
+          id: 'server.online-YAS08',
           value: true
         })
 
